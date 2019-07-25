dbca4f249ba3973a4ba8988e5460b99ca01b0bd0
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-07-25 18:38:27 -0400

    first TRYYYYYY

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -7,5 +7,9 @@ const useDarkMode = () => {
 
   useEffect( () => {
     return mode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
-  }, []);
+  }, [mode]);
+
+  return [mode, setMode];
 }
+
+export default useDarkMode;
